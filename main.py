3debedf89e98dd20e36922516b4f035f9d684825
Maxim Tislenko <makstislenko@gmail.com>
2022-09-23 07:37:17 +0400

    Добавлены графики

--- main.py
+++ main.py
@@ -40,14 +40,14 @@ def get_b_krank(k, c, alpha, I, l):
     return b
 
 def get_c_implicit(k, c, alpha, I, l, T, K):
-    c = np.arange(0, I+1, dtype=float)
+    cm = np.arange(0, I+1, dtype=float)
     h_z=l/I
     h_t=T/K
-    c[0]=1-k/(alpha*h_z)
+    cm[0]=1-k/(alpha*h_z)
     for i in range(1, I):
-        c[i]=1/h_t+2*k/(c*h_z**2)
-    c[I]=1+k/(alpha*h_z)
-    return c
+        cm[i]=1/h_t+2*k/(c*h_z**2)
+    cm[I]=1+k/(alpha*h_z)
+    return cm
 
 def get_c_krank(k, c, alpha, I, l, T, K):
     c = np.arange(0, I+1, dtype=float)
@@ -59,7 +59,7 @@ def get_c_krank(k, c, alpha, I, l, T, K):
     c[I]=1+k/(alpha*h_z)+c*h_z/2/alpha/h_t
     return c
 
-def get_f_implicit(u_0, c, R, beta, p, a, u_k_minus_1, alpha):
+def get_f_implicit(u_0, c, R, beta, p, a, u_k_minus_1, alpha, I, T, K, l):
     f = np.arange(0, I+1, dtype=float)
     h_t=T/K
     phi = get_phi(c, R, beta, p, a, I, l)
@@ -72,27 +72,29 @@ def get_f_implicit(u_0, c, R, beta, p, a, u_k_minus_1, alpha):
 def get_phi(c, R, beta, p, a, I, l):
     integral = 0
     z = np.arange(0, l, l/I)
-    for i in range(1001):
+    for i in range(1, 1001):
         r = i/1000*R
-        integral+=p*np.exp(-(r/a)**2)/2/a**2*r
+        r_1 = (i-1)/1000*R
+        integral+=p*np.exp(-(r/a)**2)/2/a**2*r+p*np.exp(-(r_1/a)**2)/2/a**2*r_1
+    integral*=1/2000
     return 2*np.exp(-beta*z)/c/R**2
 
-def compute_implicit(I, k_thermal_cond, alpha, c, i, k, l, T, K, u_0):
-    a = get_a_implicit(k, c, alpha, I, l)
-    b = get_b_implicit(k, c, alpha, I, l)
-    c = get_c_implicit(k, c, alpha, I, l, T, K)
-    f = get_f_implicit(u_0, c, R, beta, p, a, np.full(I-1, u_0))
-    u = np.zeros((K, I))
-    u[0][:] = solving(a, b, c, f)
+def compute_implicit(I, k_thermal_cond, alpha, c, i, k, l, T, K, u_0, R, beta, p, a):
+    am = get_a_implicit(k, c, alpha, I, l)
+    bm = get_b_implicit(k, c, alpha, I, l)
+    cm = get_c_implicit(k, c, alpha, I, l, T, K)
+    fm = get_f_implicit(u_0, c, R, beta, p, a, np.full(I, u_0), alpha, I, T, K, l)
+    u = np.zeros((K, I+1))
+    u[0][:] = s.thomas_method(am, bm, cm, fm)
     for i in range(1, K):
-        f = get_f_implicit(u_0, c, R, beta, p, a, u[i - 1])
-        u[i][:]=solving(a, b, c, f)
+        fm = get_f_implicit(u_0, c, R, beta, p, a, u[i - 1], alpha, I, T, K, l)
+        u[i][:]=s.thomas_method(am, bm, cm, fm)
     return u[i][:], u[:][k]
 
 
 
 def button_clicked():
-    с = 1.35
+    c = 1.35
     l = 5
     k_thermal_cond=0.065
     alpha = 0.008
@@ -133,9 +135,9 @@ def button_clicked():
         p = (float)(form.lineEdit_13.text())
     if (form.lineEdit_14.text() != ''):
         a = (float)(form.lineEdit_14.text())*R
-    u_i, u_k = compute_implicit(I, k_thermal_cond, alpha, c, i, k, l, T, K, u_0)
-    zs = np.arange(0, l, l/I)
-    ts = np.arange(0, T, T / K)
+    u_i, u_k = compute_implicit(I, k_thermal_cond, alpha, c, i, k, l, T, K, u_0,R, beta, p ,a)
+    zs = np.arange(0, l, l/(I+1))
+    ts = np.arange(0, T, T / (K+1))
     plotWidget = pg.plot(title="График для фиксированного i="+(str)(i))
     plotWidget2 = pg.plot(title="График для фиксированного k="+(str)(k))
     plotWidget.plot(zs, u_k)  ## setting pen=(i,3) automaticaly creates three different-colored pens
