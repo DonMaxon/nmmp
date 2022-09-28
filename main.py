2614870bb250311b1465f24c5b0b4299b5fc0e8f
Maxim Tislenko <makstislenko@gmail.com>
2022-09-28 14:51:53 +0400

    Добавлены графики

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ def get_phi(c, R, beta, p, a, I, l):
         r_1 = (i-1)/1000*R
         integral+=p*np.exp(-(r/a)**2)/2/a**2*r+p*np.exp(-(r_1/a)**2)/2/a**2*r_1
     integral*=1/2000
-    return 2*np.exp(-beta*z)/c/R**2
+    return beta*2*np.exp(-beta*z)/c/R**2
 
 def get_phi_krank(c, R, beta, p, a, I, l):
     integral = 0
@@ -106,7 +106,9 @@ def compute_implicit(I, k_thermal_cond, alpha, c, i, k, l, T, K, u_0, R, beta, p
     cm = get_c_implicit(k, c, alpha, I, l, T, K)
     fm = get_f_implicit(u_0, c, R, beta, p, a, np.full(I, u_0), alpha, I, T, K, l)
     u = np.zeros((K, I+1))
-    u[0, :] = s.thomas_method(am, bm, cm, fm)
+
+    #u[0, :] = s.thomas_method(am, bm, cm, fm)
+    u[0, :] = u_0
     for i in range(1, K):
         fm = get_f_implicit(u_0, c, R, beta, p, a, u[i - 1, :], alpha, I, T, K, l)
         u[i, :]=s.thomas_method(am, bm, cm, fm)
@@ -118,7 +120,8 @@ def compute_krank(I, k_thermal_cond, alpha, c, i, k, l, T, K, u_0, R, beta, p, a
     cm = get_c_krank(k, c, alpha, I, l, T, K)
     fm = get_f_krank(u_0, c, R, beta, p, a, np.full(I+1, u_0), alpha, I, T, K, l, k)
     u = np.zeros((K, I+1))
-    u[0, :] = s.thomas_method(am, bm, cm, fm)
+    #u[0, :] = s.thomas_method(am, bm, cm, fm)
+    u[0, :] = u_0
     for i in range(1, K):
         fm = get_f_krank(u_0, c, R, beta, p, a, u[i - 1, :], alpha, I, T, K, l, k)
         u[i, :]=s.thomas_method(am, bm, cm, fm)
